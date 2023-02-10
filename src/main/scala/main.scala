6a4a5dfda2f621ba7c33bd93599ee377bfd2079e
Hartmut P <hartmut0407@googlemail.com>
2023-02-10 16:41:14 +0100

    ok

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,16 +1,20 @@
 package com.cetalison.id_u8nv97m416f3
 
-import scala.io.AnsiColor._
+import Util.leftpad
 
+import scala.io.AnsiColor.*
 
 @main
-def main(): Unit =
-  
-  // \u001b[${nn}m
+def main(): Unit = {
+
+  // \u001b[${nn}m    
   
   for n <- 1 to 128 do
-    val f = s"\u001b[${n}m"
-    val g = s"$RESET" + f + "Lorem" + s"$RESET" + " "
-    print(g)
-  
-  
+    val ansicode = s"\u001b[${n}m"
+    val descr = leftpad(
+      payload = n.toString,
+      target_len = 3)
+    val token = s"$RESET" + ansicode + descr + s"$RESET" + " "
+    print(token)
+
+}
