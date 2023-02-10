f552b65d8c1d91859024f3325b1d62c79fd9cf57
Hartmut P <hartmut0407@googlemail.com>
2023-02-10 16:49:14 +0100

    ok

--- src/main/scala/Util.scala
+++ src/main/scala/Util.scala
@@ -1,13 +1,13 @@
 package com.cetalison.id_u8nv97m416f3
 
 object Util:
-  
+
   def leftpad(
-               payload: String, target_len: Int, padding_char: Char = ' '): String =
+      payload: String,
+      target_len: Int,
+      left_padding_char: Char = ' '
+  ): String =
     val padding_number = target_len - payload.length
     assert(padding_number >= 0)
-    val fill = padding_char.toString * padding_number
+    val fill = left_padding_char.toString * padding_number
     fill + payload
-
-   
-   
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -7,13 +7,14 @@ import scala.io.AnsiColor.*
 @main
 def main(): Unit = {
 
-  // \u001b[${nn}m    
-  
+  // \u001b[${nn}m
+
   for n <- 1 to 128 do
     val ansicode = s"\u001b[${n}m"
     val descr = leftpad(
       payload = n.toString,
-      target_len = 3)
+      target_len = 3
+    )
     val token = s"$RESET" + ansicode + descr + s"$RESET" + " "
     print(token)
 
