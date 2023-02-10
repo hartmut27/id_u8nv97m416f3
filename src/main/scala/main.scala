f64a3a7e94a6c0ef854663c835043b7ce44f3dcc
Hartmut P <hartmut0407@googlemail.com>
2023-02-10 16:07:35 +0100

    ok

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,6 +1,13 @@
 package com.cetalison.id_u8nv97m416f3
 
+import scala.io.AnsiColor._
+
+
 @main
-def main(): Unit = {
-  println("Hello world!")
-}
+def main(): Unit =
+
+  for x <- 1 to 128 do
+    val nn = "%02d".format(x)
+    println(s"$RESET\\u001b[${nn}m \u001b[${nn}m...Beispiel...$RESET")
+  
+  
