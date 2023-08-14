8da8e9a09e024fe29f6d767f3f755d9a761b7727
Hartmut P <hartmut0407@googlemail.com>
2023-08-14 15:23:39 +0200

    ok

--- src/main/scala/com/cetalison/id_u8nv97m416f3/Util.scala
+++ src/main/scala/com/cetalison/id_u8nv97m416f3/Util.scala
@@ -2,13 +2,14 @@ package com.cetalison.id_u8nv97m416f3
 
 object Util:
 
-  // ID_TQ4CWNQF3TDP
+  // id_tq4cwnqf3tdp
+  // E.g.: payload ="12" Len = 3 Output=" 12"
   def leftpad(
       payload: String,
       target_len: Int,
       left_padding_char: Char = ' '
   ): String =
-  
+
     val padding_number =
       target_len - payload.length
     assert(padding_number >= 0)
